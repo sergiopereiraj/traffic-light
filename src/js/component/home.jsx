4134ced8c0e41db9f075e1979e65096a36415967
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-11-06 18:48:34 +0000

    guardado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-//gitimport { useState } from "react";
+//import rigoImage from "../../img/rigo-baby.jpg";
+//import { useState } from "react";
 
 //create your first component
 export function Home() {
 	const [selectedColor, setSelectedColor] = useState("yellow");
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={
-					"light red" + (selectedColor === "red") ? "glow" : ""
-				}></div>
-			<div
-				onClick={() => setSelectedColor("yellow")}
-				className={
-					"light yellow" + (selectedColor === "yellow") ? "glow" : ""
-				}></div>
-			<div
-				onClick={() => setSelectedColor("green")}
-				className={
-					"light green" + (selectedColor === "green") ? "glow" : ""
-				}></div>
-		</div>
+		<>
+			<div className="stick"></div>
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor === "green" ? " glow" : "")
+					}></div>
+			</div>
+		</>
 	);
 }
 
